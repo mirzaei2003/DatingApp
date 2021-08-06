531724402dd2d1d6de53fed10f5e09377d468045
hossein <mhmirzaei.me@gmail.com>
2021-08-06 23:14:00 +0430

    end of section

---
+++ DatiangApp/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatiangApp.Data;
+using DatiangApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatiangApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _Context;
+        public UsersController(DataContext context)
+        {
+            _Context = context;
+        }
+
+        [HttpGet]
+        public async Task< ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _Context.Users.ToListAsync();
+           
+        }
+
+        [HttpGet("{id}")]
+        public async Task< ActionResult<AppUser>> GetUser(int id)
+        {
+          return await _Context.Users.FindAsync(id);
+            
+        }
+
+    }
+}
---
+++ DatiangApp/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using DatiangApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatiangApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
